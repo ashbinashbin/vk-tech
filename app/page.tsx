333c64e8581d8d8d39d45a28526c171ff37af692
ashbinashbin <ashbinashbin117@gmail.com>
2026-02-02 16:32:47 +0530

    tex-palning section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/components/navbar";
 import Header from "@/components/Header";
 import PopularCourses from "@/components/PopularCourses"
-
+import TaxPlanningSection from "@/components/TaxPlanningSection"
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
       <PopularCourses />
+      <TaxPlanningSection/>
     </>
   );
 }
